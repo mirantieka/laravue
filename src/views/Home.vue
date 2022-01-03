6e078ca4503b382ab2e4979123ddd3c495cc9a7c
mirantieka <mirantiekaa4@gmail.com>
2022-01-03 15:09:03 +0700

    upload frontend project

---
+++ src/views/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+    <MainHeader />
+    <MainCarousel />
+    <ProductCarousel />
+    <Insta />
+    <Partner />
+    <MainFooter />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from "@/components/HelloWorld.vue";
+import MainHeader from '../components/MainHeader.vue';
+import MainCarousel from '../components/MainCarousel.vue';
+import ProductCarousel from '../components/ProductCarousel.vue';
+import Insta from '../components/Insta.vue';
+import Partner from '../components/Partner.vue';
+import MainFooter from '../components/MainFooter.vue';
+
+export default {
+  name: "Home",
+  components: {
+    MainHeader,
+    MainCarousel,
+    ProductCarousel,
+    Insta,
+    Partner,
+    MainFooter
+  },
+};
+</script>
