10ae49ee839df0fb66ad0246cda48250415ede49
mirantieka <mirantiekaa4@gmail.com>
2022-02-04 12:00:29 +0700

    separating file from dashboard

--- laravel-backend/resources/views/includes/navbar.blade.php
+++ laravel-backend/resources/views/includes/navbar.blade.php
@@ -16,7 +16,11 @@
                 </a>
 
                 <div class="user-menu dropdown-menu">
-                    <a class="nav-link" href="#"><i class="fa fa-power -off"></i>Logout</a>
+                    <a class="nav-link" href="#" onclick="event.preventDefault(); document.getElementById('logout-form').submit();"><i class="fa fa-power -off"></i>Logout</a>
+                
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        @csrf
+                    </form>
                 </div>
             </div>
 
